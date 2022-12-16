f9d9db39796eac6dfaf0ce6923db8011a0a46d6e
seoyeon <50821216+seoyeon9646@users.noreply.github.com>
2022-12-16 14:13:29 +0900

    add 'kiwipiepy.corpus' to 'packages'

--- setup.py
+++ setup.py
@@ -215,7 +215,7 @@ setup(
     install_requires=[
         'kiwipiepy_model~=0.14'
     ],
-    packages=['kiwipiepy'],
+    packages=['kiwipiepy', 'kiwipiepy.corpus'],
     include_package_data=True,
     ext_modules=[CMakeExtension('_kiwipiepy',
         libraries=libraries,
